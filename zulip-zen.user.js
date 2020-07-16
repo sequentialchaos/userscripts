87a42478d87d685cdd39e63fc397812a077aa901
Winston Smith <wsbraves@gmail.com>
2020-07-16 16:05:13 -0400

    change tabs to spaces in metadata

--- zulip-zen.user.js
+++ zulip-zen.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name     			Zulip Zen Script
-// @description 	A zen-like view for Zulip, toggle sidebars with 'z'.
-// @author				Winston Smith <winston@sequential.me> (sequential.me)
-// @author				blinry <sebastian@morr.cc> (morr.cc)
-// @version  			0.1.0
-// @include  			https://*.zulip.com/*
-// @include  			https://*.zulipchat.com/*
-// @include       https://chat.zulip.org/*
-// @updateURL     https://github.com/sequentialchaos/userscripts/raw/master/zulip-zen.user.js
-// @icon 					https://zulip.com/static/images/favicon.png
-// @grant    			none
+// @name           Zulip Zen Script
+// @description    A zen-like view for Zulip, toggle sidebars with 'z'.
+// @author         Winston Smith <winston@sequential.me> (sequential.me)
+// @author         blinry <sebastian@morr.cc> (morr.cc)
+// @version        0.1.0
+// @include        https://*.zulip.com/*
+// @include        https://*.zulipchat.com/*
+// @include        https://chat.zulip.org/*
+// @updateURL      https://github.com/sequentialchaos/userscripts/raw/master/zulip-zen.user.js
+// @icon           https://zulip.com/static/images/favicon.png
+// @grant          none
 // ==/UserScript==
 
 var zenToggled = true
