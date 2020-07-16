0f02f76a3253abd992aa60515bb989267a748779
Winston Smith <wsbraves@gmail.com>
2020-07-16 16:35:08 -0400

    make zenToggled persistent
    Co-authored-by: blinry <sebastian@morr.cc>

--- zulip-zen.user.js
+++ zulip-zen.user.js
@@ -3,31 +3,40 @@
 // @description    A zen-like view for Zulip, toggle sidebars with 'z'.
 // @author         Winston Smith <winston@sequential.me> (sequential.me)
 // @author         blinry <sebastian@morr.cc> (morr.cc)
-// @version        0.1.0
+// @version        0.2.0
 // @include        https://*.zulip.com/*
 // @include        https://*.zulipchat.com/*
 // @include        https://chat.zulip.org/*
 // @updateURL      https://github.com/sequentialchaos/userscripts/raw/master/zulip-zen.user.js
 // @icon           https://zulip.com/static/images/favicon.png
-// @grant          none
+// @run-at         document-start
+// @grant          GM.getValue
+// @grant          GM.setValue
 // ==/UserScript==
 
-var zenToggled = true
-var style = document.createElement('style')
-style.type = 'text/css'
-style.textContent = `
-.column-left, .right-sidebar-items {
-	display: none;
-}`
-document.head.appendChild(style)
+;(async () => {
+	let zenToggled = await GM.getValue('zenToggled', false)
 
-document.addEventListener('keydown', (e) => {
-	if (e.key == 'z') {
-		if (zenToggled) {
-			style.remove()
-		} else {
-			document.head.appendChild(style)
-		}
-		zenToggled = !zenToggled
+	var style = document.createElement('style')
+	style.type = 'text/css'
+	style.textContent = `
+  .column-left, .right-sidebar-items {
+    display: none;
+  }`
+
+	if (zenToggled) {
+		document.head.appendChild(style)
 	}
-})
+
+	document.addEventListener('keydown', (e) => {
+		if (e.key == 'z') {
+			if (zenToggled) {
+				style.remove()
+			} else {
+				document.head.appendChild(style)
+			}
+			zenToggled = !zenToggled
+			GM.setValue('zenToggled', zenToggled)
+		}
+	})
+})()
